ac9c8ff77dc77e39d0f409d505585f090b00c4c7
Hipxel-dev <79396924+Hipxel-dev@users.noreply.github.com>
2025-10-23 11:14:59 +0700

    variable.py : Percobaan memakai Array di Python
    Percobaan dari Apta Saniy Razan untuk memakai Array di data mahasiswa
    
    Saya memakai Array untuk menulis data mahasiswa. dan Array untuk menulis kehadiran siswa dalam 10 hari. 
    
    Apakah ini dibolehkan jika teknik init dipakai di perusahaan beneran?

--- 01-variabel-tipe-data/variabel.py
+++ 01-variabel-tipe-data/variabel.py
@@ -24,3 +24,35 @@ print(nama1)
 kehadiran_fauzi = ['H', 'H', 'I', 'H', 'A', 'H']
 print(kehadiran_fauzi)
 print(kehadiran_fauzi[2])
+
+
+
+class data_mahasiswa :
+    def __init__(self,nim,nama,kehadiran):
+        self.nim = nim
+        self.nama = nama
+        self.kehadiran = kehadiran
+
+# 0 = Nim, 1 = Nama, 2 = Kehadiran
+["h","h","h","h","h","h","h","h","h","h",]
+data = [
+    [6113671,"Ultas Uniya", ["h","h","h","h","h","h","h","h","h","h",]],
+    [2567113,"Namias Utita", ["h","h","h","h","h","h","h","h","h","h",]],
+    [1133888,"Iopsdf Ids Edadi", ["h","h","h","h","h","h","h","h","h","h",]],
+    [7484482,"Edadian Adudian", ["h","h","h","h","h","h","h","h","h","h",]],
+    [1125533,"Yayiyuye Yayyi Sulaiman", ["h","h","h","h","h","h","h","h","h","h",]],
+    [5672188,"Uiye Aiyi Aggogo", ["h","h","h","h","h","h","h","h","h","h",]],
+]
+
+for i in range(len(data)):
+    data_mahasiswa = data[i]
+    print(str(data[i][0]) + data[i][1] + data[i][2])
+
+
+
+
+
+
+
+
+
